fcd4c643902d3d8af703056b7c48041554ba87a0
LavanyaaCSE <142470176+LavanyaaCSE@users.noreply.github.com>
2023-11-29 14:23:42 +0530

    Update LandingSection.jsx

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Hello, I am Lavanyaa!";
+const bio1 = "A fullstack developer";
+const bio2 = "specialised in React and Django";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
